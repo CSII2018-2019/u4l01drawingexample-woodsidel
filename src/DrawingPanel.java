ab69812f646b047ca57ac238f3f1e479ddc6c473
woodsidel <woodsidel@LR064WA7-1718.ellis.int>
2019-03-13 12:56:28 -0400

    FINISHED LOGO

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -1,13 +1,98 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
 public class DrawingPanel extends JPanel {
 	public DrawingPanel() {
+		setBackground(Color.CYAN);
 		
 	}
 	
+	public void paintComponent(Graphics g) {
+		/*//g.setColor(Color.MAGENTA);
+		//g.drawString("This is a string", 200, 250);
+		
+		//eyes
+		g.setColor(Color.BLUE);
+		g.drawLine(100, 100, 100, 150);
+		g.drawLine(150, 100, 150, 150);
+		
+		//mouth
+		//g.fillArc(100, 75, 50, 90, 180);
+		//Picture an oval with upper left corner of the screen
+		//100, 75, and its 50 wide and 50 high
+		g.setColor(Color.RED);
+		g.fillArc(100, 200, 50, 50, 180, 180);
+		
+		g.setColor(Color.RED);*/
+		
+		
+		int initialSpotX = 50;
+		int initialSpotY = 50;
+		for(int i = 0; i < 10; i++) {
+			Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(3));
+            initialSpotX++;
+            initialSpotY++;
+            Font titleFont = new  Font ("Harlow Solid Italic", Font.BOLD, 40);
+    		g.setFont(titleFont);
+            g.drawString("LCW", 90, 150);
+            g.setColor(new Color(255, 0, 164));
+			g.drawOval(initialSpotX, initialSpotY, 180, 180);
+			initialSpotX = resetSpot(initialSpotX);
+			initialSpotY = resetSpot(initialSpotY);
+			
+		}
+		for(int i = 0; i < 10; i++) {
+			g.setColor(Color.BLACK);
+			Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(3));
+			initialSpotX += 10;
+			g.drawOval(initialSpotX, initialSpotY, 180, 180);
+			initialSpotX = resetSpot(initialSpotX);
+			initialSpotY = resetSpot(initialSpotY);
+		}
+		for(int i = 0; i < 10; i++) {
+			g.setColor(Color.BLACK);
+			Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(3));
+			initialSpotX -= 10;
+			g.drawOval(initialSpotX, initialSpotY, 180, 180);
+			initialSpotX = resetSpot(initialSpotX);
+			initialSpotY = resetSpot(initialSpotY);
+		}
+		for(int i = 0; i < 10; i++) {
+			g.setColor(Color.BLACK);
+			Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(3));
+			initialSpotY += 10;
+			g.drawOval(initialSpotX, initialSpotY, 180, 180);
+			initialSpotX = resetSpot(initialSpotX);
+			initialSpotY = resetSpot(initialSpotY);
+		}
+		for(int i = 0; i < 10; i++) {
+			g.setColor(Color.BLACK);
+			Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(3));
+			initialSpotY -= 10;
+			g.drawOval(initialSpotX, initialSpotY, 180, 180);
+			initialSpotX = resetSpot(initialSpotX);
+			initialSpotY = resetSpot(initialSpotY);
+		}
+		
+		
+	}
+	private static int resetSpot(int spot) {
+		spot = 50;
+		return spot;
+	}
+
 	public Dimension getPreferredSize() {
-		return new Dimension(250, 200);
+		return new Dimension(500, 500);
 	}
 }
--- src/U4L01DrawingGraphics.java
+++ src/U4L01DrawingGraphics.java
@@ -11,29 +11,18 @@ import javax.swing.UIManager;
 public class U4L01DrawingGraphics extends JFrame {
 	public U4L01DrawingGraphics(){
         initGUI();
-        
-        setTitle("Drawing Example");
         setResizable(false);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
-        //setSize(200, 200);
+        int WIDTH = 300;
+        int HEIGHT = 300;
+        setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 	
 	 private void initGUI(){
-	        //title 
-	        JPanel titlePanel = new JPanel(); 
-	        titlePanel.setBackground(Color.BLACK);
-	        add(titlePanel, BorderLayout.PAGE_START);
-	        Font titleFont = new Font("Fish&Chips", Font.BOLD, 32);
-	        JLabel titleLabel = new JLabel("Drawing Examples");
-	        titleLabel.setBackground(Color.BLACK);
-	        titleLabel.setOpaque(true);
-	        titleLabel.setForeground(Color.WHITE);
-	        titleLabel.setFont(titleFont);
-	        titleLabel.setHorizontalAlignment(JLabel.CENTER);
-	        titlePanel.add(titleLabel);
+	       
 	        
 	        //drawing panel
 	        DrawingPanel dp = new DrawingPanel();
